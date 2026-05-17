517c541fccd8aa7d1a8380709e866defa05e899c
Agent610 <BhavsarParthD96@Gmail.com>
2026-05-17 16:03:56 -0400

    Starting the page

--- gaming-dashboard-frontend/src/components/App/App.jsx
+++ gaming-dashboard-frontend/src/components/App/App.jsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "../Layout/AppLayout.jsx";
+
+function Dashboard() {
+  return <h1>Dashboard Page</h1>;
+}
+
+function Community() {
+  return <h1>Community Page</h1>;
+}
+
+function Profile() {
+  return <h1>Profile Page</h1>;
+}
+
 function App() {
   return (
-    <div>
-      <h1>Gaming Dashboard</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="community" element={<Community />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+    </Routes>
   );
 }
 
