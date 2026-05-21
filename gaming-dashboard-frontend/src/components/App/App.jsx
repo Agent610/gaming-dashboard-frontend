2db3354eb8edf79d653aaea8f4f3a9d5c9d2c1d9
Agent610 <BhavsarParthD96@Gmail.com>
2026-05-21 19:37:34 -0400

    Corrected App.jsx

--- gaming-dashboard-frontend/src/components/App/App.jsx
+++ gaming-dashboard-frontend/src/components/App/App.jsx
@@ -1,68 +1,52 @@
 import { Routes, Route } from "react-router-dom";
+
 import AppLayout from "../Layout/AppLayout.jsx";
 import Dashboard from "../Dashboard/Dashboard.jsx";
 import Login from "../Login/Login.jsx";
 import Register from "../Register/Register.jsx";
+import Community from "../Community/Community.jsx";
+import Profile from "../Profile/Profile.jsx";
+import Settings from "../Settings/Settings.jsx";
+
 import ProtectedRoute from "../ProtectedRoute.jsx";
-
-// function Dashboard() {
-//   return <h1>Dashboard Page</h1>;
-// }
-
-function Community() {
-  return <h1>Community Page</h1>;
-}
-
-function Profile() {
-  return <h1>Profile Page</h1>;
-}
-
-function Settings() {
-  return <h1>Settings Pages</h1>;
-}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route
-          index
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="community"
-          element={
-            <ProtectedRoute>
-              <Community />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="settings"
-          element={
-            <ProtectedRoute>
-              <Settings />
-            </ProtectedRoute>
-          }
-        />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="community" element={<Community />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="settings" element={<Settings />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+
+// // function Dashboard() {
+// //   return <h1>Dashboard Page</h1>;
+// // }
+
+// function Community() {
+//   return <h1>Community Page</h1>;
+// }
+
+// function Profile() {
+//   return <h1>Profile Page</h1>;
+// }
+
+// function Settings() {
+//   return <h1>Settings Pages</h1>;
+// }
